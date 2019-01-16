7aa58ce1eeb5b09ab5f375e8aa61b01a9e7d87a4
Osama Mostafa <44681678+momomen@users.noreply.github.com>
2019-01-16 19:26:06 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -35,14 +35,6 @@ document.getElementById('equal').onclick = function() {
     
 };
 
-    document.getElementById('clear-button').onclick = function () {
-    doc.innerHTML = '';
+document.getElementById('clear-button').onclick = function () {
+        doc.innerHTML = '';
 };
-
-/*
-function randColor(){
-    return '#' + (function co(lor){   
-        return (lor +=
-        [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
-        && (lor.length == 6) ?  lor : co(lor); })('');
-}*/
